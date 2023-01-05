d88ad0ef91f06c8be4dd92a1dc034a562a45d0ec
ngocduy1202 <duydang9902@gmail.com>
2023-01-05 10:57:05 +0700

    Initial commit

---
+++ src/test/java/com/demo/authen/DemoAuthenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.authen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAuthenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
